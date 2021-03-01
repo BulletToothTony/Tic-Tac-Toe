92a60d8f606a2d2494da9c7efb1eab2ca9e6424d
Henry WL <henrywl@gmail.com>
2021-03-01 22:51:06 +0000

    Gameboard and player setup

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+
+const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+    return {
+        board
+    }
+})();
+
+const player = (name) => {
+    const move = () => console.group(move + name)
+}
