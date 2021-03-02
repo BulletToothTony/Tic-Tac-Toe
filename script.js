471943bb9eb7995c227c95f3009fb054ce38f264
Henry WL <henrywl@gmail.com>
2021-03-02 22:09:58 +0000

    Styling

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+// Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. 
+// If you need multiples of something (players!), create them with factories.
 
 
-
-
+// module
 const gameBoard = (() => {
-    const board = ["", "", "", "", "", "", "", "", ""];
+    const board = ["X", "O", "X", "O", "X", "X", "X", "X", "O"];
     return {
         board
     }
 })();
 
+
 const player = (name) => {
-    const move = () => console.group(move + name)
+    const move = () => console.log(`my name is ${name}`)
+    return {move}
 }
+
+function renderGameBoard() {
+    gameBoard()
+    for (let i = 0; i< board.length; i++) {
+        console.log(i)
+    }
+}
+
+renderGameBoard()
